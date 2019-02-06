3ecf833d95998eb27fec243e26089040c88cbe04
Charvát Michal <charvat@avast.com>
2019-02-06 14:58:20 +0100

    BREAKING CHANGE: Renamed original "withFallBackTo" fallback strategy to "withFallbackIfError" and introduced new "withFallbackIfNotFound" fallback strategy.

--- core/src/main/scala/com/avast/clients/storage/storage.scala
+++ core/src/main/scala/com/avast/clients/storage/storage.scala
@@ -17,7 +17,7 @@ package object storage {
   }
 
   implicit class StorageBackendOps[F[_]: Monad](val backend: StorageBackend[F]) {
-    def withFallbackTo(fallback: StorageBackend[F]): StorageBackend[F] = new StorageBackend[F] {
+    def withFallbackIfError(fallback: StorageBackend[F]): StorageBackend[F] = new StorageBackend[F] {
       override def head(sha256: Sha256): F[Either[StorageException, HeadResult]] = {
         EitherT(backend.head(sha256))
           .orElse(EitherT(fallback.head(sha256)))
@@ -28,6 +28,27 @@ package object storage {
         EitherT(backend.get(sha256, dest))
           .orElse(EitherT(fallback.get(sha256, dest)))
           .value
+      }
+
+      override def close(): Unit = {
+        backend.close()
+        fallback.close()
+      }
+    }
+
+    def withFallbackIfNotFound(fallback: StorageBackend[F]): StorageBackend[F] = new StorageBackend[F] {
+      override def head(sha256: Sha256): F[Either[StorageException, HeadResult]] = {
+        EitherT(backend.head(sha256)).flatMap[StorageException, HeadResult] {
+          case r: HeadResult.Exists => EitherT.rightT(r)
+          case HeadResult.NotFound => EitherT(fallback.head(sha256))
+        }.value
+      }
+
+      override def get(sha256: Sha256, dest: File): F[Either[StorageException, GetResult]] = {
+        EitherT(backend.get(sha256, dest)).flatMap[StorageException, GetResult] {
+          case r: GetResult.Downloaded => EitherT.rightT(r)
+          case GetResult.NotFound => EitherT(fallback.get(sha256, dest))
+        }.value
       }
 
       override def close(): Unit = {
